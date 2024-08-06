b021e7d62a8f47a5bb5d29e03f5a36358a6d4d61
S-Puranik <132969156+S-Puranik@users.noreply.github.com>
2024-08-06 12:48:33 +0530

    Create initialize.cpp

---
+++ initialize.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    char name[20]= "Siddhant Puranik";
+    char gender[20]= "Male";
+    int age = 18;
+    char city[20]= "Kalyan";
+    float height = 5.10;
+    cout << "Name: "<<name << "\n"<< "Gender: "<<gender << "\n" <<"Age: " <<age << "\n" <<"City: " <<city << "\n" << "Height: "<<height;
+
+    return 0;
+}
