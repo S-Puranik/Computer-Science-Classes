0e4a5621e2d3e781a37cadbe05adeb10fee71131
S-Puranik <132969156+S-Puranik@users.noreply.github.com>
2024-08-12 23:57:23 +0530

    Create task_5_circle.cpp

---
+++ task_5_circle.cpp
@@ -0,0 +1,22 @@
+Executed Code: -
+// Paste the executed code here
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    float radius, area, perimeter;
+    
+    cout<< "Enter the radius of the circle in cm: ";
+    cin>> radius;
+    
+    //Calculating the area and perimeter with their respective formulaes:
+    
+    area = 3.14 * radius * radius;
+    perimeter = 2 * 3.14 * radius;
+    
+    cout<<"\n The area of the circle is: " << area <<" square cm.";
+    cout<<"\n The perimeter of the circle is: " << perimeter << " cm.";
+    return 0;
+}
